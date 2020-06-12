77d0e96012d06551391e888c1c9300e5b1e96d9f
JefvdA <jef.v.d.a@live.be>
2020-06-12 21:01:36 +0200

    fouten gefixed

--- Semester 2/Webtech/Voorbereiding examens/Rick and morty/script.js
+++ Semester 2/Webtech/Voorbereiding examens/Rick and morty/script.js
@@ -6,59 +6,64 @@ function getCharacterInfo(){
     var sType = "character/";
 
     var sInput = $("#characterInput").val();
+    var isnum = /^\d+$/.test(sInput);
 
-    $.getJSON(sBaseURL + sType)
-        .done((oData) => {
-            var oCharacters = oData.results;
-            oCharacters.forEach(character => {
-                var isnum = /^\d+$/.test(sInput);
-                if(isnum){
-                    if(character.id == sInput){
-                        makeCharacterCard(character)
-                    }
-                } else {
-                    if(character.name == sInput){
-                        makeCharacterCard(character)
-                    }
-                }
+    if(isnum){
+        $.getJSON(sBaseURL + sType + sInput)
+            .done((oData) => {
+                $("#characterCard").html("")
+                makeCharacterCard(oData);
+            })
+            .fail((oError) => {
+                alert("Something went wrong, check your spelling. If you are still not sure, check the console");
+                console.log(oError);
             });
-        })
-        .fail((oError) => {
-            alert("Something went wrong, check your spelling. If you are still not sure, check the console")
-            console.log(oError)
-        });
+    } else {
+        $.getJSON(sBaseURL + sType + "?name=" + sInput)
+            .done((oData) => {
+                var aResults = oData.results;
+                $("#characterCard").html("")
+                aResults.forEach(result => {
+                    makeCharacterCard(result)
+                });
+            })
+            .fail((oError) => {
+                alert("Something went wrong, check your spelling. If you are still not sure, check the console");
+                console.log(oError);
+            });
+    }
 }
 
 function makeCharacterCard(sCharacter){
-    $("#characterCard").html('<div class="card" style="width: 18rem;"><img class="card-img-top" src="' + sCharacter.image + '" alt="Sorry, couldnt find image for this character"><div class="card-body"><h5 class="card-title">' + sCharacter.name + '</h5><p class="card-text">Species: ' + sCharacter.species + '</p><p class="card-text">Gender: ' + sCharacter.gender + '</p><p class="card-text">Origin: ' + sCharacter.origin.name + '</p><p class="card-text">Location: ' + sCharacter.location.name + '</p></div></div>');
+    $("#characterCard").append('<div class="card" style="width: 18rem;"><img class="card-img-top" src="' + sCharacter.image + '" alt="Sorry, couldnt find image for this character"><div class="card-body"><h5 class="card-title">' + sCharacter.name + '</h5><p class="card-text">ID: ' + sCharacter.id + '<p class="card-text">Species: ' + sCharacter.species + '</p><p class="card-text">Gender: ' + sCharacter.gender + '</p><p class="card-text">Status: ' + sCharacter.status + '<p class="card-text">Origin: ' + sCharacter.origin.name + '</p><p class="card-text">Location: ' + sCharacter.location.name + '</p></div></div>');
 }
 
 
 function getLocationInfo(){
     //Declare type ( character, location, episode )
-    var sType = "location/"
+    var sType = "location/";
 
     $.getJSON(sBaseURL + sType)
         .done((odata) => {
 
         })
         .fail((oError) => {
-            alert("Something went wrong, check your spelling. If you are still not sure, check the console")
-            console.log(oError)
+            alert("Something went wrong, check your spelling. If you are still not sure, check the console");
+            console.log(oError);
         });
 }
 
 
 function getEpisodeInfo(){
     //Declare type ( character, location, episode )
-    var sType = "episode/"
+    var sType = "episode/";
 
     $.getJSON(sBaseURL + sType)
         .done((odata) => {
 
         })
         .fail((oError) => {
-            alert("Something went wrong, check your spelling. If you are still not sure, check the console")
-            console.log(oError)
+            alert("Something went wrong, check your spelling. If you are still not sure, check the console");
+            console.log(oError);
         });
     }
