1d32ac85af440f77b846e1bb8910adc0b1f46b4a
JefvdA <jef.v.d.a@live.be>
2020-06-13 12:28:05 +0200

    episode page

--- Semester 2/Webtech/Voorbereiding examens/Rick and morty/script.js
+++ Semester 2/Webtech/Voorbereiding examens/Rick and morty/script.js
@@ -89,24 +89,67 @@ function addResidentToList(sResidentUrl, nLocationID){
         .done((oData) => {
             var sResidentName = oData.name
             $("#locationResidentList_" + nLocationID).append('<ul class="list-group-item">' + sResidentName + '</ul>');
-            console.log("#locationResidentList_" + nLocationID);
         })
         .fail((oError) => {
             console.log(oError);
         })
 }
 
-
 function getEpisodeInfo(){
     //Declare type ( character, location, episode )
     var sType = "episode/";
 
-    $.getJSON(sBaseURL + sType)
-        .done((oData) => {
+    var sInput = $("#episodeInput").val();
+    var bIsnum = /^\d+$/.test(sInput);
 
+    if(bIsnum){
+        $.getJSON(sBaseURL + sType + sInput)
+            .done((oData) => {
+                $("#episodeJumbotron").html("");
+                makeEpisodeJumbotron(oData);
+
+                var aCharacters = oData.characters;
+                aCharacters.forEach(character => {
+                    addCharacterToList(character, oData.id);
+                });
+            })
+            .fail((oError) => {
+                alert("Sorry, could not find the episode, please check your input and try again. (For more info check the console)");
+                console.log(oError);
+            });
+    } else {
+        $.getJSON(sBaseURL + sType + "?name=" + sInput)
+            .done((oData) => {
+                $("#episodeJumbotron").html("")
+
+                var aResults = oData.results;
+                aResults.forEach(result => {
+                    makeEpisodeJumbotron(result);
+
+                    var aCharacters = result.characters;
+                    aCharacters.forEach(character => {
+                        addCharacterToList(character, result.id);
+                    });
+                });
+            })
+            .fail((oError) => {
+                alert("Sorry, could not find the episode, please check your spelling and try again. (For more info check the console)");
+                console.log(oError);
+            });
+    }
+}
+
+function makeEpisodeJumbotron(oEpisode){
+    $("#episodeJumbotron").append('<div class="jumbotron"><h1>' + oEpisode.name + '</h1><p class="lead">Air date: ' + oEpisode.air_date + '</p><p class="lead">Episode: ' + oEpisode.episode + '</p><hr class="my-4"><p>Characters in this episode</p><ul class="list-group" id="episodeCharacterList_' + oEpisode.id + '"></ul></div>');
+}
+
+function addCharacterToList(sCharacterUrl, nEpisodeID){
+    $.getJSON(sCharacterUrl)
+        .done((oData) => {
+            var sCharacterName = oData.name;
+            $("#episodeCharacterList_" + nEpisodeID).append('<li class="list-group-item">' + sCharacterName + '</li>');
         })
         .fail((oError) => {
-            alert("Something went wrong, check your spelling. If you are still not sure, check the console");
             console.log(oError);
         });
-    }
+}
