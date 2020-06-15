1e55dba63ae32095228e48fd49069feffc809167
JefvdA <jef.v.d.a@live.be>
2020-06-15 10:51:07 +0200

    mealDB mooier

--- Semester 2/Webtech/Voorbereiding examens/MealDB/script.js
+++ Semester 2/Webtech/Voorbereiding examens/MealDB/script.js
@@ -39,7 +39,7 @@ function processMealsData(aMeals){
         var aIngredients = [oMeal.strIngredient1, oMeal.strIngredient2, oMeal.strIngredient3, oMeal.strIngredient4, oMeal.strIngredient5, oMeal.strIngredient6, oMeal.strIngredient7, oMeal.strIngredient8, oMeal.strIngredient9, oMeal.strIngredient10, oMeal.strIngredient11, oMeal.strIngredient12, oMeal.strIngredient13, oMeal.strIngredient14, oMeal.strIngredient15, oMeal.strIngredient16, oMeal.strIngredient17, oMeal.strIngredient18, oMeal.strIngredient19, oMeal.strIngredient20];
         var aMeasures = [oMeal.strMeasure1, oMeal.strMeasure2, oMeal.strMeasure3, oMeal.strMeasure4, oMeal.strMeasure5, oMeal.strMeasure6, oMeal.strMeasure7, oMeal.strMeasure8, oMeal.strMeasure9, oMeal.strMeasure10, oMeal.strMeasure11, oMeal.strMeasure12, oMeal.strMeasure13, oMeal.strMeasure14, oMeal.strMeasure15, oMeal.strMeasure16, oMeal.strMeasure17, oMeal.strMeasure18, oMeal.strMeasure19, oMeal.strMeasure20];
         for(var i = 0; i < aIngredients.length; i++){
-            if(aIngredients[i] == "")
+            if(aIngredients[i] == "" || null)
                 break;
 
             addIngredietToList(aIngredients[i], aMeasures[i],nMealD);
@@ -49,20 +49,18 @@ function processMealsData(aMeals){
 
 function addMealCard(oMeal){
     $("#mealCards").append('\
-    <div class="card" style="width: 18rem;">\
-        <img class="card-img-top" src="' + oMeal.strMealThumb + '" alt="Bootstrap-logo">\
-        <div class="card-body">\
-            <h5 class="card-title">' + oMeal.strMeal + '</h5>\
-            <p class="card-text">MealID: ' + oMeal.idMeal + '</p>\
-            <p class="card-text">Ingredients: <p>\
-            <ul class="list-group" id="ingredientsList_' + oMeal.idMeal + '"></ul></br>\
-            <a href="' + oMeal.strYoutube +'" class="btn btn-primary" role="button" target="_blank">Youtube video</a>\
-        </div>\
+    <div class="jumbotron">\
+        <h1>' + oMeal.strMeal + '</h1>\
+        <img src="' + oMeal.strMealThumb + '" style="width: 200px;" alt="mealImage">\
+        <p class="lead">ID: ' + oMeal.idMeal + '</p>\
+        <p>Ingredients: </p>\
+        <ul class="list-group" id="ingredientsList_' + oMeal.idMeal + '"></ul></br>\
+        <a class="btn btn-primary" href="' + oMeal.strYoutube + '" target="_blank">Youtube video</a>\
     </div>');
 }
 
 function addIngredietToList(sIngredient, sMeasure, nMealID){
-    $("#ingredientsList_" + nMealID).append('<li class="list-group-item">' + sMeasure + ' Of: ' + sIngredient + '</li>');
+    $("#ingredientsList_" + nMealID).append('<li class="list-group-item">' + sMeasure + ' : ' + sIngredient + '</li>');
 }
 
 
