19831aa20a7216b6aedbfe276c4c4f0cb93e976f
JefvdA <jef.v.d.a@live.be>
2020-11-08 16:30:45 +0100

    programming H5, H1-H5

---
+++ 2020-2021/Semester 1/Programming Principles/H5/BMI met enum/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace BMI_met_enum
+{
+    class Program
+    {
+        enum Gewichten { Ondergewicht, Normaal, Overgewicht, Obees, Zwaarlijvig, Undefined}
+        static void Main(string[] args)
+        {
+            Console.Write("Wat is je lengte?  >>");
+            double lengte = double.Parse(Console.ReadLine());
+
+            Console.Write("Wat is je gewicht?  >>");
+            double gewicht = double.Parse(Console.ReadLine());
+
+            double bmi = Math.Round(gewicht / (lengte * lengte), 2);
+
+            Gewichten soortGewicht = Gewichten.Undefined;
+            if (bmi < 18.5)
+            {
+                soortGewicht = Gewichten.Ondergewicht;
+            }
+            else if (bmi < 24.9)
+            {
+                soortGewicht = Gewichten.Normaal;
+            }
+            else if (bmi < 29.9)
+            {
+                soortGewicht = Gewichten.Overgewicht;
+            }
+            else if (bmi < 39.9)
+            {
+                soortGewicht = Gewichten.Obees;
+            }
+            else
+            {
+                soortGewicht = Gewichten.Zwaarlijvig;
+            }
+
+            switch (soortGewicht)
+            {
+                case Gewichten.Ondergewicht:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"Je hebt een BMI van {bmi}");
+                    Console.WriteLine(", je hebt ondergewicht!");
+                    break;
+                case Gewichten.Normaal:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write($"Je hebt een BMI van {bmi}");
+                    Console.WriteLine(", je hebt een normaal gewicht.");
+                    break;
+                case Gewichten.Overgewicht:
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.Write($"Je hebt een BMI van {bmi}");
+                    Console.WriteLine(", overgewicht, je loopt niet echt een risico maar je mag niet verdikken");
+                    break;
+                case Gewichten.Obees:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"Je hebt een BMI van {bmi}");
+                    Console.WriteLine(", verhoogde kans op allerlei aandoeningen zoals diabetes, hartaandoeningen en rugklachten. Je zou 5 tot 10 kg moeten vermageren.");
+                    break;
+                case Gewichten.Zwaarlijvig:
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.Write($"Je hebt een BMI van {bmi}");
+                    Console.WriteLine(", Je moet dringend vermageren want je gezondheid is in gevaar (of je hebt je lengte of gewicht in verkeerde eenheid ingevoerd).");
+                    break;
+                default:
+                    Console.WriteLine("Er is een fout opgetreden met het berekenen van de bmi, probeer opnieuw");
+                    break;
+            }
+
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2020-2021/Semester 1/Programming Principles/H5/Kleurcode weerstand ENUM/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace Kleurcode_weerstand_ENUM
+{
+    class Program
+    {
+        enum Kleuren { Zilver=-2, Goud, Zwart, Bruin, Rood, Oranje, Geel, Groen, Blauw, Paars, Grijs, Wit, Undefined}
+        static void Main(string[] args)
+        {
+            Console.Write("Kleur van de eerste ring     >>");
+            string input1 = Console.ReadLine().ToLower();
+            Console.Write("Kleur van de tweede ring     >>");
+            string input2 = Console.ReadLine().ToLower();
+            Console.Write("Kleur van de derede ring     >>");
+            string input3 = Console.ReadLine().ToLower();
+
+            Kleuren kleur1 = Kleuren.Undefined;
+            Kleuren kleur2 = Kleuren.Undefined;
+            Kleuren kleur3 = Kleuren.Undefined;
+
+
+            int ring1 = 0;
+            int ring2 = 0;
+            int mult = 0;
+
+            //Ring 1
+            switch (input1)
+            {
+                case "zwart":
+                    kleur1 = Kleuren.Zwart;
+                    break;
+                case "bruin":
+                    kleur1 = Kleuren.Bruin;
+                    break;
+                case "rood":
+                    kleur1 = Kleuren.Rood;
+                    break;
+                case "oranje":
+                    kleur1 = Kleuren.Oranje;
+                    break;
+                case "geel":
+                    kleur1 = Kleuren.Geel;
+                    break;
+                case "groen":
+                    kleur1 = Kleuren.Groen;
+                    break;
+                case "blauw":
+                    kleur1 = Kleuren.Blauw;
+                    break;
+                case "paars":
+                    kleur1 = Kleuren.Paars;
+                    break;
+                case "grijs":
+                    kleur1 = Kleuren.Grijs;
+                    break;
+                case "wit":
+                    kleur1 = Kleuren.Wit;
+                    break;
+            }
+            ring1 = (int)kleur1;
+
+            //Ring 2
+            switch (input2)
+            {
+                case "zwart":
+                    kleur2 = Kleuren.Zwart;
+                    break;
+                case "bruin":
+                    kleur2 = Kleuren.Bruin;
+                    break;
+                case "rood":
+                    kleur2 = Kleuren.Rood;
+                    break;
+                case "oranje":
+                    kleur2 = Kleuren.Oranje;
+                    break;
+                case "geel":
+                    kleur2 = Kleuren.Geel;
+                    break;
+                case "groen":
+                    kleur2 = Kleuren.Groen;
+                    break;
+                case "blauw":
+                    kleur2 = Kleuren.Blauw;
+                    break;
+                case "paars":
+                    kleur2 = Kleuren.Paars;
+                    break;
+                case "grijs":
+                    kleur2 = Kleuren.Grijs;
+                    break;
+                case "wit":
+                    kleur2 = Kleuren.Wit;
+                    break;
+            }
+            ring2 = (int)kleur2;
+
+            //Ring 3
+            switch (input3)
+            {
+                case "zwart":
+                    kleur3 = Kleuren.Zwart;
+                    break;
+                case "bruin":
+                    kleur3 = Kleuren.Bruin;
+                    break;
+                case "rood":
+                    kleur3 = Kleuren.Rood;
+                    break;
+                case "oranje":
+                    kleur3 = Kleuren.Oranje;
+                    break;
+                case "geel":
+                    kleur3 = Kleuren.Geel;
+                    break;
+                case "groen":
+                    kleur3 = Kleuren.Groen;
+                    break;
+                case "blauw":
+                    kleur3 = Kleuren.Blauw;
+                    break;
+                case "paars":
+                    kleur3 = Kleuren.Paars;
+                    break;
+                case "goud":
+                    kleur3 = Kleuren.Goud;
+                    break;
+                case "zilver":
+                    kleur3 = Kleuren.Zilver;
+                    break;
+            }
+            mult = (int)kleur3;
+
+            double totaal = int.Parse(ring1.ToString() + ring2.ToString()) * Math.Pow(10, mult);
+            Console.Clear();
+            Console.WriteLine($"Deze weerstand heeft een waarde van {totaal} Ohm.");
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
